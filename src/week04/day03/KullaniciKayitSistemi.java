632a91c9d398d2438f50406f44055594af41cc0f
alperenikinci <alperen.ikinci@hotmail.com>
2024-07-19 14:42:09 +0300

    Ders arasi, kullanici kaydi icin gerekli bilgiler metot icerisinden alindi.

--- src/week04/day03/KullaniciKayitSistemi.java
+++ src/week04/day03/KullaniciKayitSistemi.java
@@ -46,25 +46,115 @@ public class KullaniciKayitSistemi {
     }
 
     private static Kullanici kullaniciKaydi() {
-       String isim;
-       String soyisim;
-       String email;
-       String telNo;
-       String sifre;
-       String sifreYeniden;
-       String kullaniciAdi;
-       String tcKimlik;
        LocalDate dogumTarihi;
        Kullanici kullanici;
        dogumTarihi = dogumTarihiAl();
        if(dogumTarihKontrol(dogumTarihi)){
+           //Dogum tarihini kontrolunu gectik, o zaman kullanici olusturabiliriz.
            kullanici = new Kullanici();
            kullanici.setDogumTarihi(dogumTarihi);
 
+
+           String[] isimSoyisim = isimSoyisimAl();
+           kullanici.setIsim(isimSoyisim[0]);
+           kullanici.setSoyisim(isimSoyisim[1]);
+           kullanici.setEmail(emailAl());
+           kullanici.setTelNo(telNoAl());
+           kullanici.setSifre(sifreAl());
+           kullanici.setKullaniciAdi(kullaniciAdiAl());
+           kullanici.setTcKimlik(tcKimlikAl());
        } else{
            System.out.println("18 Yasindan kucukler kayit islemi gerceklestiremez.");
        }
         return null;
+    }
+
+    private static String tcKimlikAl() {
+        String tcKimlik;
+        while (true){
+            System.out.println("Lutfen TC kimlik numaranizi giriniz : ");
+            tcKimlik = scanner.nextLine();
+            if(tcKimlik.length()!=11){
+                System.out.println("TC Kimlik No 11 haneli olmalidir.");
+                continue;
+            } else if(!numerikDegerKontrol(tcKimlik)){
+                System.out.println("TC Kimlik No sadece numerik karakterler barindirabilir. ");
+                continue;
+            }
+            return tcKimlik;
+        }
+    }
+    private static boolean numerikDegerKontrol(String value){
+        for (int i = 0; i<value.length();i++){
+            if(!Character.isDigit(value.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static String kullaniciAdiAl() {
+
+        String kullaniciAdi;
+        while (true){
+            System.out.println("Lutfen bir kullanici adi giriniz min 4 - max 16 karakter : ");
+            kullaniciAdi = scanner.nextLine();
+            if(kullaniciAdi.length()<4){
+                System.out.println("Kullanici adi 4 karakterden kisa olamaz.");
+                continue;
+            } else if(kullaniciAdi.length()>16){
+                System.out.println("Kullanici adi 16 karakterden uzun olamaz.");
+                continue;
+            }
+            return kullaniciAdi;
+            }
+        }
+
+
+    private static String sifreAl() {
+        String sifre;
+        String sifreYeniden;
+        while (true){
+            System.out.println("Lutfen bir sifre giriniz min 8 - max 32 karakter : ");
+            sifre = scanner.nextLine();
+            if(sifre.length()<8){
+                System.out.println("Sifre 8 karakterden kisa olamaz.");
+                continue;
+            } else if(sifre.length()>32){
+                System.out.println("Sifre 32 karakterden uzun olamaz.");
+                continue;
+            } else {
+                System.out.print("Lutfen sifreninizi tekrar giriniz : ");
+                sifreYeniden = scanner.nextLine();
+                if (sifre.equals(sifreYeniden)){
+                    return sifre;
+                } else {
+                    System.out.println("Girdiginiz sifreler eslesmiyor! ");
+                }
+            }
+        }
+
+    }
+
+    private static String telNoAl() {
+        System.out.print("Lutfen telefon numaranizi giriniz : +90 ");
+        String telNo = scanner.nextLine();
+        return telNo;
+    }
+
+    private static String emailAl() {
+        System.out.print("Lutfen mailinizi giriniz : ");
+        String email = scanner.nextLine();
+        return email;
+    }
+
+    private static String[] isimSoyisimAl() {
+        String[] isimSoyisim = new String[2];
+        System.out.print("Lutfen adinizi giriniz : ");
+        isimSoyisim[0] = scanner.nextLine();
+        System.out.print("Lutfen soyadinizi giriniz : ");
+        isimSoyisim[1] = scanner.nextLine();
+        return isimSoyisim;
     }
 
     private static LocalDate dogumTarihiAl() {
